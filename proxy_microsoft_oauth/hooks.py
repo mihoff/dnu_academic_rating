5b8ea2c3ce669f01c55b68db854335d971fbb109
Michael Tkachenko <mihofff@gmail.com>
2021-09-16 22:57:10 +0300

    - added log if authentication failed

--- proxy_microsoft_oauth/hooks.py
+++ proxy_microsoft_oauth/hooks.py
@@ -1,9 +1,14 @@
+import logging
+
 from proxy_microsoft_oauth.exceptions import DNUAuthHookException
 from user_profile.models import Profile
+
+logger = logging.getLogger()
 
 
 def auth_hook(user, token):
     profile = Profile.objects.filter(user=user).first()
     if profile and profile.position is None:
+        logger.error(f"Office365 authentication failed :: {user} <{token}>")
         user.delete()
         raise DNUAuthHookException(user=user)
