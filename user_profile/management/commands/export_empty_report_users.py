9112e45d4c021694b3d0e60c55da609dd1b28a3d
Michael Tkachenko <mihofff@gmail.com>
2021-09-27 12:07:25 +0300

    - export users that were not in the system or did not fill in all reports

---
+++ user_profile/management/commands/export_empty_report_users.py
@@ -0,0 +1,38 @@
+import csv
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+from django.db.models import Q
+
+from user_profile.models import Profile
+
+
+class Command(BaseCommand):
+    help = 'Export all users to csv file. Will be placed to /tmp/export_empty_report_users_{timestamp}.csv'
+
+    def handle(self, *args, **options):
+        with open(f"/tmp/export_empty_report_users_{datetime.now().strftime('%Y_%m_%d')}.csv", 'w', newline='') as f:
+            fieldnames = ["Прізвище", "Ім'я По батькові", "Факультет", "Кафедра", "Посада", "Був в системі"]
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+            writer.writeheader()
+
+            for p in Profile.objects.filter(
+                    Q(user__genericreportdata__isnull=True) |
+                    Q(user__microsoft_account__isnull=True) |
+                    Q(user__genericreportdata__educationalandmethodicalwork__isnull=True) |
+                    Q(user__genericreportdata__scientificandinnovativework__isnull=True) |
+                    Q(user__genericreportdata__organizationalandeducationalwork__isnull=True)
+            ):
+                writer.writerow(
+                    {
+                        "Прізвище": p.user.first_name,
+                        "Ім'я По батькові": p.user.last_name,
+                        "Факультет": getattr(p.department, "faculty", None),
+                        "Кафедра": p.department,
+                        "Посада": p.position,
+                        "Був в системі": getattr(p.user, "microsoft_account", False) and "Так" or "Ні"
+                    }
+                )
+
+        self.stdout.write("Success")
