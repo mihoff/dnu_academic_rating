809bb6fc660759b578ae169c4a4567bd942f2d4c
Michael Tkachenko <mihofff@gmail.com>
2021-09-28 18:14:16 +0300

    - rollback the ability to delete UserProfile objects from admin

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -76,9 +76,6 @@ class ProfileAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return request.user.is_superuser
 
-    def has_delete_permission(self, request, obj=None):
-        return request.user.is_superuser
-
 
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
