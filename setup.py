f8d66598c2a2cf056bac2608aea6bb012cba3f71
Michael Tkachenko <m.tkachenko@pokermatch.com>
2022-12-19 17:25:39 +0100

    update Django version to the latest stable Django==3.2.16

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "Django==3.2.4",
+        "Django==3.2.16",
         "django-microsoft-auth==2.4.0",
         "pyjwt==1.7.1",
         "pylint==2.8.3",
